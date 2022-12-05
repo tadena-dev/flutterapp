e34532640fab89452bb3fa715029a3a63021742f
Emmanuel Tadena Jr <tadena.dev@gmail.com>
2022-12-06 06:46:59 +0800

    Text Button Widget

---
+++ flutter-widgets/001-Button/1-TextButton/textbutton/lib/main.dart
@@ -0,0 +1,167 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Text Button Widget',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyWidget(),
+    );
+  }
+}
+
+///Create a Stateful widget called MyWidget
+///this will be our home page or root page.
+///Replace the Container with Scaffold widget
+///give it a title under the appbar.
+///Supply a body with Center widget with a child
+///of Column widget then set the main axis alignment
+///to center then invoke the children for the column widget.
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Text Button Widget'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+
+          ///Create Text Button where we can manipulate
+          ///its UI in defferent ways
+          ///First - A plain Text Button Widget
+          ///Second - Customize the Button Style
+          ///Third - Customize the TextButton.styleFrom()
+          ///Fourth - Customize in other ways
+          children: [
+            ///Plain Text Button
+            TextButton(
+              onPressed: () {},
+              child: const Text('TextButton'),
+            ),
+
+            ///Spacer
+            const SizedBox(
+              height: 30,
+            ),
+
+            ///Customize Button Style of Text Button
+            ///Acc
+            TextButton(
+              style: ButtonStyle(
+                backgroundColor:
+                    const MaterialStatePropertyAll<Color>(Colors.grey),
+                foregroundColor:
+                    const MaterialStatePropertyAll<Color>(Colors.white),
+                shape: MaterialStatePropertyAll<OutlinedBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+                fixedSize: const MaterialStatePropertyAll<Size>(
+                  Size(150, 40),
+                ),
+              ),
+              onPressed: () {},
+              child: const Text('TextButton'),
+            ),
+
+            ///Spacer
+            const SizedBox(
+              height: 30,
+            ),
+
+            ///Customize with TextButton.styleFrom()
+            TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.grey,
+                foregroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                fixedSize: const Size(150, 40),
+              ),
+              onPressed: () {},
+              child: const Text('TextButton'),
+            ),
+
+            ///Spacer
+            const SizedBox(
+              height: 30,
+            ),
+
+            ///Customize Text Button
+            ///Creating a ClipRRect to access the borderRadius
+            ///with a child of Stack widget to stack
+            ///container widget and the textbutton widget.
+            ///The reason for this is we have variety of
+            ///option on how we decorate our container.
+            ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Stack(
+                children: [
+                  Positioned.fill(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.deepPurpleAccent,
+                            blurRadius: 20,
+                            spreadRadius: -5,
+                          ),
+                        ],
+                        gradient: const LinearGradient(
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                          colors: [
+                            Color.fromARGB(255, 252, 85, 244),
+                            Color.fromARGB(255, 130, 250, 83),
+                            Color.fromARGB(255, 80, 197, 252),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 30,
+                      vertical: 5,
+                    ),
+                    child: TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        'TextButton',
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
