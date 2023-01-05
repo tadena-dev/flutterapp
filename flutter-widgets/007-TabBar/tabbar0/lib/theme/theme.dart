f33c4b5bcf5007e81754f5e92e9c237af7d78464
Emmanuel Tadena Jr <tadena.dev@gmail.com>
2023-01-05 21:41:05 +0800

    TabBar

--- flutter-widgets/007-TabBar/tabbar0/lib/theme/theme.dart
+++ flutter-widgets/007-TabBar/tabbar0/lib/theme/theme.dart
@@ -2,7 +2,26 @@ import 'package:flutter/material.dart';
 
 final customTheme = ThemeData(
   tabBarTheme: const TabBarTheme(
-    labelColor: Colors.yellowAccent,
+    labelColor: Colors.black,
     unselectedLabelColor: Colors.greenAccent,
+    indicator: BoxDecoration(
+      shape: BoxShape.circle,
+      boxShadow: [
+        BoxShadow(
+          color: Colors.redAccent,
+          blurRadius: 20,
+          spreadRadius: 0.5,
+        ),
+      ],
+      gradient: LinearGradient(
+        begin: Alignment.center,
+        end: Alignment.bottomCenter,
+        colors: [
+          Colors.lightBlue,
+          Colors.lightGreen,
+          Colors.pinkAccent,
+        ],
+      ),
+    ),
   ),
 );
